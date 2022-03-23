5844e904ed4031b4d0875e55402de6c37bb4f0c3
Lucas Alcântara <lucashmalcantara@gmail.com>
2022-03-23 07:33:48 -0300

    Implements methods to compare Task.WaitAll and Task.WhenAll.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # waitall-vs-whenall-dotnet
+
 What is the difference between Task.WaitAll and Task.WhenAll?
+
+# Execution
+
+`Task.WaitAll` blocks the current thread until everything has completed.
+
+`Task.WhenAll` returns a *task* which represents the action of waiting until everything has completed.
+
+# Exceptions
+
+`Task.WaitAll` throws an `AggregateException` when any of the tasks throws and you can examine all thrown exceptions. The `await` in `await Task.WhenAll` unwraps the `AggregateException` and 'returns' only the first exception. In both cases, all tasks will run, even if one of them throws an exception.
+
+When the program below executes with `await Task.WhenAll(taskArray)` the output is as follows.
+
+```csharp
+23/03/2022 10:18:51: Task 1 started
+23/03/2022 10:18:51: Task 2 started
+23/03/2022 10:18:51: Task 3 started
+Caught AggregateException in Main at 23/03/2022 10:18:54: Task 1 throwing at 23/03/2022 10:18:52
+Caught AggregateException in Main at 23/03/2022 10:18:54: Task 2 throwing at 23/03/2022 10:18:53
+Caught AggregateException in Main at 23/03/2022 10:18:54: Task 3 throwing at 23/03/2022 10:18:54
+Done.
+```
+
+When the program below is executed with `Task.WaitAll(taskArray)` the output is as follows.
+
+```csharp
+23/03/2022 10:18:54: Task 1 started
+23/03/2022 10:18:54: Task 3 started
+23/03/2022 10:18:54: Task 2 started
+Caught Exception in Main at 23/03/2022 10:18:57: Task 1 throwing at 23/03/2022 10:18:55
+Done.
+```
+
+# Credits
+
+[c# - WaitAll vs WhenAll - Stack Overflow](https://stackoverflow.com/questions/6123406/waitall-vs-whenall)
---
+++ WaitAllVsWhenAll/WaitAllVsWhenAll/Program.cs
@@ -0,0 +1,78 @@
+﻿using WaitAllVsWhenAll;
+
+static void WaitAndThrow(int id, int waitInMs)
+{
+    Console.WriteLine($"{DateTime.UtcNow}: Task {id} started");
+
+    Thread.Sleep(waitInMs);
+    throw new CustomException($"Task {id} throwing at {DateTime.UtcNow}");
+}
+
+static void WaitAll()
+{
+    Console.WriteLine("\n\n# Task.WaitAll");
+
+    try
+    {
+        Task[] taskArray = { Task.Factory.StartNew(() => WaitAndThrow(1, 1000)),
+                                 Task.Factory.StartNew(() => WaitAndThrow(2, 2000)),
+                                 Task.Factory.StartNew(() => WaitAndThrow(3, 3000)) };
+
+        Task.WaitAll(taskArray);
+        Console.WriteLine("This isn't going to happen");
+    }
+    catch (AggregateException ex)
+    {
+        foreach (var inner in ex.InnerExceptions)
+        {
+            Console.WriteLine($"Caught AggregateException in Main at {DateTime.UtcNow}: " + inner.Message);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Caught Exception in Main at {DateTime.UtcNow}: " + ex.Message);
+    }
+
+    Console.WriteLine("Done.");
+}
+
+
+static async Task WhenAll()
+{
+    try
+    {
+        Console.WriteLine("\n\n# Task.WhenAll");
+
+        Task[] taskArray = { Task.Factory.StartNew(() => WaitAndThrow(1, 1000)),
+                                 Task.Factory.StartNew(() => WaitAndThrow(2, 2000)),
+                                 Task.Factory.StartNew(() => WaitAndThrow(3, 3000)) };
+
+        await Task.WhenAll(taskArray);
+        Console.WriteLine("This isn't going to happen");
+    }
+    catch (AggregateException ex)
+    {
+        foreach (var inner in ex.InnerExceptions)
+        {
+            Console.WriteLine($"Caught AggregateException in Main at {DateTime.UtcNow}: " + inner.Message);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Caught Exception in Main at {DateTime.UtcNow}: " + ex.Message);
+    }
+
+    Console.WriteLine("Done.");
+}
+
+
+Console.WriteLine("###### Task.WaitAll vs Task.WhenAll ######");
+
+WaitAll();
+
+Task.Run(async () =>
+{
+    await WhenAll();
+}).Wait();
+
+Console.ReadLine();
